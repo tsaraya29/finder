a3a8e055612beb6e20731ad46cbfa5d579449a79
tsaraya29 <saratgrffn@gmail.com>
2019-03-10 18:33:41 -0500

    combined js scripts, need to seperate later

--- server.js
+++ server.js
@@ -18,8 +18,36 @@ const PORT = process.env.PORT || 3000;
 
 
 
+// ROUTES
+// Below is a series of express routes.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
 
+// When this route is 'hit' we respond with the survey.html file
+APP.get("/survey", function(req, res){
+	res.sendFile(PATH.join(__dirname, "./app/public/survey.html"));
+});
+
+//Default catch all that leads to home.html
+APP.get("/", function(req, res){   
+    res.sendFile(PATH.join(__dirname, "./app/public/home.html"));
+});
+
+//This is the routes for lthe survey
+APP.get("/api/friends", function(req, res){
+	res.json(friends);
+});
+
+
+
+// This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. When this route is 'hit' we console log the posted data then
+// redirect to user back to the main route. (above, which sends the home.html file)
+APP.post("/api/friends", function(req, res){
+	console.log(req.body);
+
+	res.redirect("/");
+});
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
