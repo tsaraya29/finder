10e89b662e45024cf4f6d4cc2408dd368c50c63f
T Saraya <saratgrffn@gmail.com>
2019-03-19 15:19:46 -0500

    almost had it

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,10 +14,43 @@ APP.get("/api/friends", function(req, res){
 
 // This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. When this route is 'hit' we console log the posted data then redirect to user back to the main route. 
 APP.post("/api/friends", function(req, res){
-	friends.push(req.body);
-	res.json(true);
+	
+	// comparison logic here///
+	// user1 will be req.body.score 
+	// create for loop that loops thru friends array
+	//least
+
+	var bestMatch = {
+		name: "",
+		photo: "",
+		diff: 1000
+	};
+
+	var friendData = req.body;
+	var userScores = friendData.scores;
+
+	console.log(userScores);
+
+	var totalDiff = 0;
+		for(var i = 0; i < friends.length; i++) {
+		 console.log(friends[i]);
+		 totalDiff = 0;
+		 
+		 //nested loop
+		 for (var j = 0; j < friends[i].scores; j++){
+			 totalDiff += Math.abs(parseInt(userScores[j] - parseInt(friends[i])));
+			 //if the sum of the difference in scores is less than difference of the current match
+			 if (totalDiff <= bestMatch.diff){
+				bestMatch.name = friends[i].name;
+				bestMatch.photo = friends[i].photo;
+				bestMatch.diff = totalDiff;
+			 }
+		 }
+		}
+	friends.push(friendData);
+	res.json(bestMatch); // returns data back to front end application
 	console.log(req.body);
 
-	res.redirect("/");
+	
 });
 };	
